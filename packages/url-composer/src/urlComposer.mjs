f0a87fffd0f32faeb38752f70cb093e15366bd7b
Hugo Nogueira <hugo.nogueira@lego.com>
2023-06-04 18:45:42 +0100

    chore: tidy urlComposer

---
+++ packages/url-composer/src/urlComposer.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+
+/**
+ * @typedef {Object.<string, string>} mappping - A case sensitive key value pair
+ */
+
+/**
+ * @param {mappping} mappping - An object with key value pairs to replace in the url
+ * @returns {import('lightningcss').Visitor} - A visitor that replaces the url
+ * @example
+ * const res = transform({
+ *  minify: true,
+ *  code: Buffer.from("body { background: url(${URL}/logo.svg); }"),
+ *  visitor: composeVisitors([urlComposer({ URL: "https://sardine.dev" })]),
+ * });
+ *
+ * assert.strictEqual(
+ *  res.code.toString(),
+ *  "body{background:url(https://sardine.dev/logo.svg)}",
+ *  new TypeError("Replaced URL is not the same")
+ * );
+ */
+export default (mappping) => ({
+  /**
+   * @param {import('lightningcss').Url} url - The url object to transform
+   * @returns {import('lightningcss').Url} - The transformed url object
+   */
+  Url(url) {
+    for (const [key, value] of Object.entries(mappping)) {
+      const replacementKey = `\${${key}}`;
+      if (url.url.includes(replacementKey)) {
+        url.url = url.url.replace(replacementKey, value);
+        return url;
+      }
+    }
+  },
+});
--- packages/url-composer/src/urlComposer.test.mjs
+++ packages/url-composer/src/urlComposer.test.mjs
@@ -5,11 +5,11 @@ import urlComposer from "./urlComposer.mjs";
 const res = transform({
   minify: true,
   code: Buffer.from("body { background: url(${URL}/logo.svg); }"),
-  visitor: composeVisitors([urlComposer({ URL: "https://lego.com" })]),
+  visitor: composeVisitors([urlComposer({ URL: "https://sardine.dev" })]),
 });
 
 assert.strictEqual(
   res.code.toString(),
-  "body{background:url(https://lego.com/logo.svg)}",
+  "body{background:url(https://sardine.dev/logo.svg)}",
   new TypeError("Replaced URL is not the same")
 );
