c6fbab3cf8f9bc5db080e95a20a9d2c018161d51
Hugo Nogueira <hugo.nogueira@lego.com>
2024-07-07 19:02:07 +0100

    add vit and vitest

--- packages/url-composer/src/urlComposer.test.ts
+++ packages/url-composer/src/urlComposer.test.ts
@@ -1,61 +1,47 @@
-import assert from "node:assert/strict";
-import { it, describe } from "node:test";
+import { expect, it } from "vitest";
 import { composeVisitors, transform } from "lightningcss";
 import urlComposer from "./urlComposer";
 
-describe("urlComposer", () => {
-	it("should replace the url", () => {
-		const res = transform({
-			minify: true,
-			code: Buffer.from("body { background: url(${URL}/logo.svg); }"),
-			visitor: composeVisitors([urlComposer({ URL: "https://sardine.dev" })]),
-		});
+it("should replace the url", () => {
+  const res = transform({
+    filename: "test.css",
+    minify: true,
+    code: Buffer.from("body { background: url(${URL}/logo.svg); }"),
+    visitor: composeVisitors([urlComposer({ URL: "https://sardine.dev" })]),
+  });
 
-		const actual = res.code.toString();
-		const expected = "body{background:url(https://sardine.dev/logo.svg)}";
+  const actual = res.code.toString();
+  const expected = "body{background:url(https://sardine.dev/logo.svg)}";
 
-		assert.strictEqual(
-			actual,
-			expected,
-			`Expected ${actual} to be ${expected}`,
-		);
-	});
+  expect(actual).toBe(expected);
+});
 
-	it("should replace the url with multiple keys", () => {
-		const res = transform({
-			minify: true,
-			code: Buffer.from(
-				"body { background: url(${URL}/${VERSION}/logo.svg); }",
-			),
-			visitor: composeVisitors([
-				urlComposer({ URL: "https://sardine.dev", VERSION: "v1" }),
-			]),
-		});
+it("should replace the url with multiple keys", () => {
+  const res = transform({
+    filename: "test.css",
+    minify: true,
+    code: Buffer.from("body { background: url(${URL}/${VERSION}/logo.svg); }"),
+    visitor: composeVisitors([
+      urlComposer({ URL: "https://sardine.dev", VERSION: "v1" }),
+    ]),
+  });
 
-		const actual = res.code.toString();
-		const expected = "body{background:url(https://sardine.dev/v1/logo.svg)}";
+  const actual = res.code.toString();
+  const expected = "body{background:url(https://sardine.dev/v1/logo.svg)}";
 
-		assert.strictEqual(
-			actual,
-			expected,
-			`Expected ${actual} to be ${expected}`,
-		);
-	});
+  expect(actual).toBe(expected);
+});
 
-	it("shouldn't replace the url if the case doesn't match", () => {
-		const res = transform({
-			minify: true,
-			code: Buffer.from("body { background: url(${URL}/logo.svg); }"),
-			visitor: composeVisitors([urlComposer({ url: "https://sardine.dev" })]),
-		});
+it("shouldn't replace the url if the case doesn't match", () => {
+  const res = transform({
+    filename: "test.css",
+    minify: true,
+    code: Buffer.from("body { background: url(${URL}/logo.svg); }"),
+    visitor: composeVisitors([urlComposer({ url: "https://sardine.dev" })]),
+  });
 
-		const actual = res.code.toString();
-		const expected = "body{background:url(${URL}/logo.svg)}";
+  const actual = res.code.toString();
+  const expected = "body{background:url(${URL}/logo.svg)}";
 
-		assert.strictEqual(
-			actual,
-			expected,
-			`Expected ${actual} to be ${expected}`,
-		);
-	});
+  expect(actual).toBe(expected);
 });
