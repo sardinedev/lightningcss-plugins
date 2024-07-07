3694dcdad19b585c129fedbea0e0e2693f81081c
Hugo Nogueira <hugo.nogueira@lego.com>
2024-07-07 20:23:05 +0100

    add full pipeline

--- packages/url-composer/src/urlComposer.test.ts
+++ packages/url-composer/src/urlComposer.test.ts
@@ -1,47 +1,47 @@
-import { expect, it } from "vitest";
 import { composeVisitors, transform } from "lightningcss";
+import { expect, it } from "vitest";
 import urlComposer from "./urlComposer";
 
 it("should replace the url", () => {
-  const res = transform({
-    filename: "test.css",
-    minify: true,
-    code: Buffer.from("body { background: url(${URL}/logo.svg); }"),
-    visitor: composeVisitors([urlComposer({ URL: "https://sardine.dev" })]),
-  });
+	const res = transform({
+		filename: "test.css",
+		minify: true,
+		code: Buffer.from("body { background: url(${URL}/logo.svg); }"),
+		visitor: composeVisitors([urlComposer({ URL: "https://sardine.dev" })]),
+	});
 
-  const actual = res.code.toString();
-  const expected = "body{background:url(https://sardine.dev/logo.svg)}";
+	const actual = res.code.toString();
+	const expected = "body{background:url(https://sardine.dev/logo.svg)}";
 
-  expect(actual).toBe(expected);
+	expect(actual).toBe(expected);
 });
 
 it("should replace the url with multiple keys", () => {
-  const res = transform({
-    filename: "test.css",
-    minify: true,
-    code: Buffer.from("body { background: url(${URL}/${VERSION}/logo.svg); }"),
-    visitor: composeVisitors([
-      urlComposer({ URL: "https://sardine.dev", VERSION: "v1" }),
-    ]),
-  });
+	const res = transform({
+		filename: "test.css",
+		minify: true,
+		code: Buffer.from("body { background: url(${URL}/${VERSION}/logo.svg); }"),
+		visitor: composeVisitors([
+			urlComposer({ URL: "https://sardine.dev", VERSION: "v1" }),
+		]),
+	});
 
-  const actual = res.code.toString();
-  const expected = "body{background:url(https://sardine.dev/v1/logo.svg)}";
+	const actual = res.code.toString();
+	const expected = "body{background:url(https://sardine.dev/v1/logo.svg)}";
 
-  expect(actual).toBe(expected);
+	expect(actual).toBe(expected);
 });
 
 it("shouldn't replace the url if the case doesn't match", () => {
-  const res = transform({
-    filename: "test.css",
-    minify: true,
-    code: Buffer.from("body { background: url(${URL}/logo.svg); }"),
-    visitor: composeVisitors([urlComposer({ url: "https://sardine.dev" })]),
-  });
+	const res = transform({
+		filename: "test.css",
+		minify: true,
+		code: Buffer.from("body { background: url(${URL}/logo.svg); }"),
+		visitor: composeVisitors([urlComposer({ url: "https://sardine.dev" })]),
+	});
 
-  const actual = res.code.toString();
-  const expected = "body{background:url(${URL}/logo.svg)}";
+	const actual = res.code.toString();
+	const expected = "body{background:url(${URL}/logo.svg)}";
 
-  expect(actual).toBe(expected);
+	expect(actual).toBe(expected);
 });
--- packages/url-composer/vite.config.ts
+++ packages/url-composer/vite.config.ts
@@ -2,18 +2,18 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  test: {
-    coverage: {
-      reporter: ["lcovonly", "text-summary"],
-      include: ["src/**/*.ts"],
-    },
-  },
-  build: {
-    minify: false,
-    lib: {
-      entry: resolve(__dirname, "src/urlComposer.ts"),
-      formats: ["es"],
-      fileName: "index",
-    },
-  },
+	test: {
+		coverage: {
+			reporter: ["lcovonly", "text-summary"],
+			include: ["src/**/*.ts"],
+		},
+	},
+	build: {
+		minify: false,
+		lib: {
+			entry: resolve(__dirname, "src/urlComposer.ts"),
+			formats: ["es"],
+			fileName: "urlComposer",
+		},
+	},
 });
