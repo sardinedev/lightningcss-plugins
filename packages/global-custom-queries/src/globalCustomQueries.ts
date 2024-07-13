9fe4020c4bac64682ed9f35151e3ef3e8b9f459f
Hugo Nogueira <hugo.nogueira@lego.com>
2024-07-13 13:35:39 +0100

    chore: Resolve Custom Media Queries from global file

--- packages/global-custom-queries/src/globalCustomQueries.ts
+++ packages/global-custom-queries/src/globalCustomQueries.ts
@@ -1,2 +1,56 @@
-type Options = {};
-export default (options: Options) => ({});
+import { bundle } from "lightningcss";
+import type { Declaration, MediaQuery, StyleSheet } from "lightningcss";
+
+export type Options = {
+	/* The path to the file you want to extract the custom queries from */
+	source: string;
+};
+
+function returnAST(path: string): StyleSheet<Declaration, MediaQuery> {
+	let ast: StyleSheet<Declaration, MediaQuery> | null = null;
+	bundle({
+		filename: path,
+		drafts: {
+			customMedia: true,
+		},
+		visitor: {
+			StyleSheet(stylesheet) {
+				ast = stylesheet;
+			},
+		},
+	});
+	if (!ast) {
+		throw new Error(
+			"[@sardine/lightningcss-plugin-global-custom-queries]: Could not find Media Queries in the file",
+		);
+	}
+	return ast;
+}
+
+export default ({ source }: Options) => {
+	const ast = returnAST(source);
+	return {
+		MediaQuery(query: MediaQuery): MediaQuery {
+			if (
+				query?.condition?.type === "feature" &&
+				query.condition.value.name.startsWith("--")
+			) {
+				const matchedConditionValue = query.condition.value;
+				let resolvedQuery = null;
+				for (const rule of ast.rules) {
+					if (
+						rule.type === "custom-media" &&
+						rule.value.name === matchedConditionValue.name
+					) {
+						resolvedQuery = rule.value.query.mediaQueries[0];
+						break;
+					}
+				}
+				if (resolvedQuery?.condition) {
+					query.condition = resolvedQuery.condition;
+				}
+			}
+			return query;
+		},
+	};
+};
