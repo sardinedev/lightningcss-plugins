8979f85e4dd94ced4689ca34aa31c3f18cf09646
Hugo Nogueira <hugo.nogueira@lego.com>
2026-02-23 15:37:49 +0000

    improve tests

--- packages/global-custom-queries/src/globalCustomQueries.test.ts
+++ packages/global-custom-queries/src/globalCustomQueries.test.ts
@@ -1,125 +1,106 @@
 import path from "node:path";
+import type { CustomAtRules, TransformOptions } from "lightningcss";
 import { composeVisitors, transform } from "lightningcss";
-import { expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import globalCustomQueries from "./globalCustomQueries";
 
-it("should resolve custom queries", () => {
-	const source = `
-		@media (--breakpoint) {
-			.foo {
-				color: red;
-			}
-		}
-	`;
+const BRANDED_ERROR_PREFIX = "[@sardine/lightningcss-plugin-global-custom-queries]:";
 
-	const result = "@media (width<=100em){.foo{color:red}}";
-
-	const mockFile = path.join(__dirname, "./mocks/custom-media.css");
-
-	const { code } = transform({
+/** Run lightningcss transform with the common test defaults. */
+function runTransform(source: string, options: Partial<TransformOptions<CustomAtRules>>) {
+	return transform({
 		filename: "test.css",
-		code: new TextEncoder().encode(source),
 		minify: true,
-		visitor: composeVisitors([globalCustomQueries({ source: mockFile })]),
-	});
-
-	expect(code.toString()).toBe(result);
-});
-
-it("shouldn't resolve custom queries if custom media is not in the source file", () => {
-	const source = `
-		@media (--small-breakpoint) {
-			.foo {
-				color: red;
-			}
-		}
-	`;
-
-	const result = "@media (--small-breakpoint){.foo{color:red}}";
-
-	const mockFile = path.join(__dirname, "./mocks/custom-media.css");
-
-	const { code } = transform({
-		filename: "test.css",
 		code: new TextEncoder().encode(source),
-		minify: true,
-		visitor: composeVisitors([globalCustomQueries({ source: mockFile })]),
+		...options,
 	});
+}
 
-	expect(code.toString()).toBe(result);
-});
+const customMediaFile = path.join(__dirname, "./mocks/custom-media.css");
+const noCustomMediaFile = path.join(__dirname, "./mocks/no-custom-media.css");
 
-it("shouldn't replace media queries if the source file is empty", () => {
-	const source = `
-		@media (--breakpoint) {
-			.foo {
-				color: red;
-			}
-		}
-	`;
-
-	const result = "@media (--breakpoint){.foo{color:red}}";
-
-	const mockFile = path.join(__dirname, "./mocks/no-custom-media.css");
-
-	const { code } = transform({
-		filename: "test.css",
-		code: new TextEncoder().encode(source),
-		minify: true,
-		visitor: composeVisitors([globalCustomQueries({ source: mockFile })]),
-	});
-
-	expect(code.toString()).toBe(result);
-});
-
-it("should resolve custom queries with multiple media queries", () => {
-	const source = `
-		.foo {
-			color: red;
+describe("globalCustomQueries", () => {
+	it("should resolve a known custom media query", () => {
+		const source = `
 			@media (--breakpoint) {
-				color: blue;
+				.foo { color: red; }
 			}
-		}
-		
-
-		@media (--breakpoint) {
-			.bar {
-				color: blue;
-			}
-		}
-	`;
-
-	const result = ".foo{color:red;@media (width<=100em){color:#00f}}@media (width<=100em){.bar{color:#00f}}";
-
-	const mockFile = path.join(__dirname, "./mocks/custom-media.css");
-
-	const { code } = transform({
-		filename: "test.css",
-		code: new TextEncoder().encode(source),
-		minify: true,
-		visitor: composeVisitors([globalCustomQueries({ source: mockFile })]),
+		`;
+		const { code } = runTransform(source, {
+			visitor: composeVisitors([globalCustomQueries({ source: customMediaFile })]),
+		});
+		expect(code.toString()).toBe("@media (width<=100em){.foo{color:red}}");
 	});
 
-	expect(code.toString()).toBe(result);
-});
+	it("should leave an unknown custom media query unchanged", () => {
+		const source = `
+			@media (--small-breakpoint) {
+				.foo { color: red; }
+			}
+		`;
+		// --small-breakpoint is not defined in custom-media.css
+		const { code } = runTransform(source, {
+			visitor: composeVisitors([globalCustomQueries({ source: customMediaFile })]),
+		});
+		expect(code.toString()).toBe("@media (--small-breakpoint){.foo{color:red}}");
+	});
 
-it("should throw an error if the custom media queries are not found", () => {
-	const source = `
-		@media (--breakpoint) {
+	it("should leave custom media queries unchanged when the source file is empty", () => {
+		const source = `
+			@media (--breakpoint) {
+				.foo { color: red; }
+			}
+		`;
+		const { code } = runTransform(source, {
+			visitor: composeVisitors([globalCustomQueries({ source: noCustomMediaFile })]),
+		});
+		expect(code.toString()).toBe("@media (--breakpoint){.foo{color:red}}");
+	});
+
+	it("should resolve the same custom query used multiple times", () => {
+		const source = `
 			.foo {
 				color: red;
+				@media (--breakpoint) { color: blue; }
 			}
-		}
-	`;
+			@media (--breakpoint) {
+				.bar { color: blue; }
+			}
+		`;
+		const { code } = runTransform(source, {
+			visitor: composeVisitors([globalCustomQueries({ source: customMediaFile })]),
+		});
+		expect(code.toString()).toBe(
+			".foo{color:red;@media (width<=100em){color:#00f}}@media (width<=100em){.bar{color:#00f}}",
+		);
+	});
 
-	const mockFile = path.join(__dirname, "./mocks/no-file.css");
+	it("should resolve multiple distinct custom queries in a single pass", () => {
+		const source = `
+			@media (--breakpoint) {
+				.small { color: red; }
+			}
+			@media (--large-breakpoint) {
+				.large { color: blue; }
+			}
+		`;
+		const { code } = runTransform(source, {
+			visitor: composeVisitors([globalCustomQueries({ source: customMediaFile })]),
+		});
+		expect(code.toString()).toBe("@media (width<=100em){.small{color:red}}@media (width>=120em){.large{color:#00f}}");
+	});
 
-	expect(() =>
-		transform({
-			filename: "test.css",
-			code: new TextEncoder().encode(source),
-			minify: true,
-			visitor: composeVisitors([globalCustomQueries({ source: mockFile })]),
-		}),
-	).toThrowError();
+	it("should throw a branded error when the source file does not exist", () => {
+		const source = `
+			@media (--breakpoint) {
+				.foo { color: red; }
+			}
+		`;
+		const missingFile = path.join(__dirname, "./mocks/no-file.css");
+		expect(() =>
+			runTransform(source, {
+				visitor: composeVisitors([globalCustomQueries({ source: missingFile })]),
+			}),
+		).toThrowError(BRANDED_ERROR_PREFIX);
+	});
 });
