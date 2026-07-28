a8b88ee04caf256b15f3f6dbeb954ebe559e1fd3
Hugo Nogueira <hugo.nogueira@lego.com>
2026-07-28 17:32:29 +0100

    scaffolding

---
+++ packages/custom-property-fallback/README.md
@@ -0,0 +1,7 @@
+# @sardine/lightningcss-plugin-custom-property-fallback
+
+A [Lightning CSS](https://lightningcss.dev/) plugin that does things to CSS.
+
+## Use case
+
+
---
+++ packages/custom-property-fallback/vite.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vitest/config";
+export default defineConfig({
+	test: {
+		coverage: {
+			reporter: ["lcovonly", "text-summary"],
+			include: ["src/**/*.ts"],
+			exclude: ["src/**/*.bench.ts"],
+		},
+		benchmark: {
+			include: ["src/**/*.bench.ts"],
+		},
+	},
+	build: {
+		minify: false,
+		target: "node20",
+		lib: {
+			entry: resolve(__dirname, "src/globalComposes.ts"),
+			formats: ["es"],
+			fileName: "globalComposes",
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			external: ["lightningcss", "node:path"],
+		},
+	},
+});
